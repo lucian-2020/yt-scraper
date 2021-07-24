1a89c8ae750db85142424c3e01e5002a4ad7f91e
LucianPy <lucian.pyc@gmail.com>
2021-07-24 17:11:10 +0300

    changing all caps variables names to lowercase

--- app.py
+++ app.py
@@ -2,44 +2,44 @@
 from commander import Commander
 
 if __name__ == '__main__':
-    INPUT = input('Please input a full YouTube video/playlist link:\n')
-    COMMANDER = Commander()
+    keyboard_input = input('Please input a full YouTube video/playlist link:\n')
+    commander_obj = Commander()
 
-    if 'playlist' in INPUT:
-        COMMANDER_SETTINGS = {'set_dataprocessor': 'playlist',
+    if 'playlist' in keyboard_input:
+        commander_settings = {'set_dataprocessor': 'playlist',
                               'set_scraper': 'requests'}
-        COMMANDER.execute(COMMANDER_SETTINGS)
+        commander_obj.execute(commander_settings)
 
-        PLAYLIST_SETTINGS = {'link': INPUT,
+        playlist_settings = {'link': keyboard_input,
                              'parse': True,
                              #'save': True,
                              'display': False}
-        COMMANDER.execute(PLAYLIST_SETTINGS)
-        DATA = COMMANDER.dataprocessor.current_data
+        commander_obj.execute(playlist_settings)
+        playlist_data = commander_obj.dataprocessor.current_data
 
-        COMMANDER_SETTINGS = {'set_dataprocessor': 'video',
+        commander_settings = {'set_dataprocessor': 'video',
                               'set_scraper': 'requests',
                               'set_recommender': 'simple'}
-        COMMANDER.execute(COMMANDER_SETTINGS)
+        commander_obj.execute(commander_settings)
 
-        VIDEO_SETTINGS = {'link': '',
+        video_settings = {'link': '',
                           'parse': True,
                           #'save': True,
                           'display': False,
                           'recommend': True}
-        for video in DATA:
-            VIDEO_SETTINGS['link'] = video
-            COMMANDER.execute(VIDEO_SETTINGS)
+        for video in playlist_data:
+            video_settings['link'] = video
+            commander_obj.execute(video_settings)
 
-        COMMANDER.recommend()
+        commander_obj.recommend()
 
-    elif 'watch' in INPUT:
-        COMMANDER_SETTINGS = {'set_dataprocessor': 'video',
+    elif 'watch' in keyboard_input:
+        commander_settings = {'set_dataprocessor': 'video',
                               'set_scraper': 'requests'}
-        COMMANDER.execute(COMMANDER_SETTINGS)
+        commander_obj.execute(commander_settings)
 
-        VIDEO_SETTINGS = {'link': INPUT,
+        video_settings = {'link': keyboard_input,
                           'parse': True,
                           'save': True,
                           'display': True}
-        COMMANDER.execute(VIDEO_SETTINGS)
+        commander_obj.execute(video_settings)
